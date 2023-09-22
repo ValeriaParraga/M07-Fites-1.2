f75210dc0c2e43c67f490f4cd2a5370c8d11d63e
Valeria <vparragaolivo@gmail.com>
2023-09-22 21:09:17 +0200

    Cambios en el ejercicio 2.4

--- ex24pag2.php
+++ ex24pag2.php
@@ -1,13 +1,26 @@
 <?php
 if (isset($_POST["contrasenya1"]) && isset($_POST["contrasenya2"])) {
     $sonIguals = false;
-    if ($_POST["contrasenya1"] != $_POST["contrasenya2"]) {
-        echo "ERROR: les contrasenyes han de coincidir";
+    if ($_POST["contrasenya1"] == $_POST["contrasenya2"]) {
         $sonIguals = true;
     }
 
     if($sonIguals == true) {
+        $pass = $_POST["contrasenya1"];
+        $longitud = strlen($pass);
+        $cant = 0;
+        for ($i = 0; $i < $longitud; $i++) {
+            if (is_numeric($pass[$i])) {
+                $cant += 1;
+            } 
+        }
 
+        if ($cant == 0) {
+            echo "ERROR: la contrasenya ha de tenir al menys un número";
+        }
+
+    } else {
+        echo "ERROR: les contrasenyes han de coincidir";
     }
 }
 ?>
